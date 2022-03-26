9794f9ec26c18e6f4b9ca12490fe6d73b76a91c2
BARIŞ IŞIK <20202305034@ogr.akdeniz.edu.tr>
2022-03-26 14:43:31 +0300

    26,03 Ders sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "123456";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "123645879";
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "3213456478";
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SO.L.ID 
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+        }
+    }
+}
---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //COORPORATE
+    class TuzelMusteri:Musteri
+    {
+       
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
